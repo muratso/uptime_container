e336b724afa261eb3f957b284f001a53e3efd0a1
muratso <rockstar2304@gmail.com>
2016-07-18 15:30:09 -0300

    Create install-rbenv.sh

---
+++ docker/install-rbenv.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eu
+
+su - ruby <<'EOF'
+git clone https://github.com/sstepenson/rbenv.git /home/ruby/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/ruby/.bashrc
+echo 'eval "$(rbenv init -)"' >> /home/ruby/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git /home/ruby/.rbenv/plugins/ruby-build
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+rbenv install 2.3.0
+rbenv global 2.3.0
+gem install bundler --no-ri --no-rdoc
+rbenv rerash
+EOF
